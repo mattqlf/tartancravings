6d75add498bbb2697adc8da77bbf3701c81f1c92
mattqlf <mattqlf25@gmail.com>
2025-09-30 17:16:37 -0400

    merge conflict fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TartanCravings Delivery",
+  description: "Connect with CMU delivery drivers and get orders handled fast.",
 };
 
 const geistSans = Geist({
